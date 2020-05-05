5ff9f2ef6801eae85933425fe5c3debb7a8ae931
rachmizard <rachmizard11072000@gmail.com>
2020-05-05 19:22:31 +0700

    first init

---
+++ src/app/products.ts
@@ -0,0 +1,24 @@
+export const products = [
+  {
+    name: 'iPhone XR',
+    price: 799,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    name: 'iPhone XS',
+    price: 699,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    name: 'iPhone 11',
+    price: 800,
+    description: ''
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
